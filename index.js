8e7f689e287fcbcef66cedcddc6010a7fd53ac4e
arbor <tadgranite@gmail.com>
2024-11-02 22:25:07 -0500

    fixed port error due to consolidation of repos

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
-
+const express = require('express')
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
- res.send("hello world");
-});
-app.listen(80, () => {
- console.log('Server started on port 80');
-});
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
