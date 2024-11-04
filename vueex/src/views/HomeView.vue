3fcfaa7b1088a93bfb5c691a39cb007dfbe89463
arbor <tadgranite@gmail.com>
2024-11-03 22:56:29 -0600

    changed color scheme and updated the way pages work

--- vueex/src/views/HomeView.vue
+++ vueex/src/views/HomeView.vue
@@ -8,51 +8,63 @@ const router = useRouter()
 const home = ref(null)
 const resume = ref(null)
 const thisSite = ref(null)
+const gh = ref(null)
 
-function gh () {
+const homeb = ref(null)
+const siteb = ref(null)
+const resumeb = ref(null)
+const ghb = ref(null)
+
+function send_gh () {
+  send(gh,ghb)
   window.location.href = 'https://github.com/PaperLightbulb'
 }
 
-function send_home () {
-  console.log(home.value.style)
-  home.value.style.display = "block"
+function send (sel, selb) {
+  home.value.style.display = "none"
   resume.value.style.display = "none"
   thisSite.value.style.display = "none"
+  gh.value.style.display = "none"
+
+  sel.value.style.display = "block"
+  
+  homeb.value.style.fontSize = ""
+  siteb.value.style.fontSize = ""
+  resumeb.value.style.fontSize = ""
+  ghb.value.style.fontSize = ""
+
+  selb.value.style.fontSize = "xx-large"
+}
+
+function send_home () {
+  send(home,homeb)
 }
 
 function send_resume () {
-  home.value.style.display = "none"
-  resume.value.style.display = "block" 
-  thisSite.value.style.display = "none"
+  send(resume,resumeb)
 }
 
 function send_this_site () {
-  home.value.style.display = "none"
-  resume.value.style.display = "none" 
-  thisSite.value.style.display = "block"
+  send(thisSite,siteb)
 }
 
 onMounted(() => {
-  home.value.style.display = "block"
-  resume.value.style.display = "none" 
-  thisSite.value.style.display = "none"
+  send(home,homeb)
 })
 </script>
 
 <template>
   <div class="verti">
     <h3>Showcase</h3>
-    <h1>Tim Garnett</h1>
+    <h1>Arbor Garnett</h1>
     <div class="horiz">
-      <button class="lnk" @click="send_home()"><h2>Home</h2></button>
-      <button class="lnk" @click="send_this_site()" ><h2>This Site</h2></button>
-      <button class="lnk" @click="gh()" ><h2>Github</h2></button>
-      <button class="lnk" @click="send_resume()" ><h2>Resume</h2></button>
+      <button class="lnk" @click="send_home()"><h2 ref="homeb">Home</h2></button>
+      <button class="lnk" @click="send_this_site()" ><h2 ref="siteb">This Site</h2></button>
+      <button class="lnk" @click="send_gh()" ><h2 ref="ghb">Github</h2></button>
+      <button class="lnk" @click="send_resume()" ><h2  ref="resumeb">Resume</h2></button>
     </div>
-    <br>
     <div ref="home">
-      <h2>Home</h2>
-      <p>Hello, I'm Tim.<br><br>
+      <p>Hello, I'm Arbor.<br><br>
         
       I'm excited about<br>
       - Cloud computing<br>
@@ -64,15 +76,14 @@ onMounted(() => {
       Nice to meet you!</p>
     </div>
     <div ref="resume">
-      <h2>Resume</h2>
     </div>
     <div ref="thisSite" >
-      <h2>This Site</h2>
       <p>built with<br>
       - k3s Kubernetes (Self Hosted)<br>
       - Docker<br>
       - Vue JS<br>
       - Pinggy.io (Reverse Tunnel)</p>
     </div>
+    <div ref="gh"></div>
   </div>
 </template>
