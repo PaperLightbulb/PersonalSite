a44d26333da76c43c0e9d656ec8546c64fcb7470
Arbor <tadgranite@gmail.com>
2024-12-06 17:45:02 -0600

    setting up linktree site

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useTemplateRef, ref, onMounted } from 'vue';
+
+
+
+
+</script>
+
+<template>
+  <div class="verti">
+    <h1>Timothy Garnett</h1>
+    <div class="horiz">
+      <a href="https://iloike.xyz/">Projects</a>
+      <a href="https://www.linkedin.com/in/timothy-garnett/">LinkedIn</a>
+      <a href="https://umn.joinhandshake.com/profiles/49585155">Handshake</a>
+    </div>
+  </div>
+</template>
